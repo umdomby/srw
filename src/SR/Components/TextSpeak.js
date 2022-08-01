6c84dbb77ad89e728ef6b1b47d685bd998073745
navatar <umdomby@yandex.ru>
2022-08-01 22:26:16 +0300

    edit

--- src/SR/Components/TextSpeak.js
+++ src/SR/Components/TextSpeak.js
@@ -1,11 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import store from "../Store";
 import {observer} from "mobx-react-lite";
-import { useSpeechSynthesis } from 'react-speech-kit';
+import { useSpeechSynthesis, useSpeechRecognition } from 'react-speech-kit';
 
 const TextSpeak = observer(() => {
     const { speak } = useSpeechSynthesis();
     const [value, setValue] = useState('');
+
+    const { listen, listening, stop } = useSpeechRecognition({
+        onResult: (result) => {
+            setValue(result);
+        },
+    });
+
     const handleSubmit = (e) => {
         e.preventDefault()
         store.webSocket.send(JSON.stringify({
@@ -17,9 +24,19 @@ const TextSpeak = observer(() => {
     }
 
     useEffect(()=> {
-        console.log('22222', store.textSpeak)
+        console.log('from react server', store.textSpeak)
         speak({ text: store.textSpeak })
     },[store.textSpeak])
+
+    // useEffect(()=> {
+    //     speak({ text: value })
+    //     store.webSocket.send(JSON.stringify({
+    //         id: store.idSocket,
+    //         method: 'textSpeak',
+    //         text: value,
+    //     }))
+    //     //setValue('')
+    // },[value])
 
     return (
         <div className="Dictaphone">
@@ -29,6 +46,22 @@ const TextSpeak = observer(() => {
               onChange={(event) => setValue(event.target.value)}
               onKeyPress={(event) => event.key === "Enter" && handleSubmit(event)}
           />
+            {/*<textarea*/}
+            {/*    value={value}*/}
+            {/*    onChange={(event) => setValue(event.target.value)}*/}
+            {/*/>*/}
+            {/*<button onMouseDown={listen} onMouseUp={stop}>*/}
+            {/*    🎤*/}
+            {/*</button>*/}
+            <button onClick={listen}>
+                🎤
+            </button>
+            <button onClick={stop}>
+                stop
+            </button>
+            {listening && <div>Go ahead I'm listening</div>}
+
+
         </div>
     );
 });
