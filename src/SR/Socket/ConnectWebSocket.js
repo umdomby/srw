d1daaedf84255bb29777a32e5a5a450bdceda675
navatar <umdomby@yandex.ru>
2022-08-13 10:11:13 +0300

    edit

--- src/SR/Socket/ConnectWebSocket.js
+++ src/SR/Socket/ConnectWebSocket.js
@@ -8,6 +8,7 @@ const ConnectWebSocket = observer(() => {
     const persId = useRef(shortId.generate())
     const [idSocket, setIdSocket] = useState(localStorage.getItem('localIdSocket') || '')
     setInterval(() => socketTest(), 5000)
+    const [hidden, setHidden] = useState(true)
 
     useEffect(()=>{
         if( localStorage.getItem('localIdSocket') === null || localStorage.getItem('localIdSocket') === undefined) {
@@ -45,17 +46,23 @@ const ConnectWebSocket = observer(() => {
 
     return (
         <div className="socketAdd">
+            {hidden &&
                 <input
                        type='text'
                        disabled={false}
-                       style={{backgroundColor: '#D3D3D3', textAlign: 'center', borderWidth: 1, fontSize: 10, width:'15em', height:'1.1em'}}
+                       style={{backgroundColor: '#D3D3D3', textAlign: 'center', borderWidth: 1, fontSize: 12, width:'15em', height:'1.5em'}}
                        value={idSocket}
                        onChange={(event) => {
                            localStorage.setItem('localIdSocket', event.target.value)
                            setIdSocket(event.target.value)
                            store.setIdSocket(event.target.value)
                        }}
-                />
+                />}
+            <button style={{backgroundColor: '#D3D3D3', textAlign: 'center', borderWidth: 1, fontSize: 11, width:'4em', height:'2em'}}
+                onClick={()=>setHidden(!hidden)}
+            >
+                Hid
+            </button>
             {/*<div>*/}
             {/*    <button onClick={connectID}>Connect</button>*/}
             {/*</div>*/}
